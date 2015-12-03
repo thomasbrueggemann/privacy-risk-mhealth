02a38b81bf63cecb758e9c19e1f07a434e440844
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-12-03 22:50:45 +0100

    preparing index generation

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-![mHealth](http://www.xcubelabs.com/blogs/wp-content/uploads/mhealth-03.png)
+# Node / ReactJS Template
+A template for a ReactJS webapp that is being served by a node.js express server.
 
-# Privacy Index mHealth Apps
-UI for paper "A Privacy Index for mHealth Apps". Easily compare privacy risks between apps
+Also includes:
 
-### Building Blocks
+* Bootstrap
+* jQuery
+* Sweetalert
+* MomentJS
 
-* Transpose HTML Table http://jsfiddle.net/XKnKL/3/
+## Install
+
+In home directory of project:
+
+```
+npm install
+bower install
+````
+
+## Develop
+
+Compile and watch changes in files with
+
+```
+gulp
+```
+
+from home directory. Simultaneously open another shell and serve the nodejs express application with
+
+```
+npm run watch
+```
---
+++ privacyidx.js
@@ -0,0 +1,26 @@
+var fs = require("fs");
+var csv = require("fast-csv");
+
+var ratings = [];
+
+fs.createReadStream("data.csv")
+    .pipe(csv())
+    .on("data", function(data){
+
+        // add rating from csv file
+        ratings.push({
+            "id": parseInt(data[0]),
+            "name": data[1],
+            "personal_category": data[2].split(","),
+            "login": !(data[3] === "" || data[3] === "None"),
+            "personal_target": data[4].split(","),
+            "unspecific_target": data[5].split(","),
+            "personal_description": data[6]  === "1",
+            "rating_source": data[7].split(","),
+            "data_reasonable": data[8] === "1",
+            "secure_transmission": data[9] === "1"
+        });
+    })
+    .on("end", function(){
+        console.log(ratings);
+    });
